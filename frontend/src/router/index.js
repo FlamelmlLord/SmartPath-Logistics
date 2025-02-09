1284c4968033d5ed080036dd2c2510a8c282c0d8
DanielaAlarcon <daniela08052003@gmail.com>
2025-02-08 20:45:03 -0500

    Vista paquetes, agregada (por adaptar a Base de Datos)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,17 +3,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Conductores from '../views/Conductores.vue' // Importa el componente
 import Vehiculos from '../views/Vehiculos.vue' // Importa el componente
+import Paquetes from '../views/Paquetes.vue' // Importa el componente
+
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   {
     path: '/conductores',
@@ -24,6 +21,11 @@ const routes = [
     path: '/vehiculos',
     name: 'vehiculos',
     component: Vehiculos
+  },
+  {
+    path: '/paquetes',
+    name: 'paquetes',
+    component: Paquetes
   }
 ]
 
